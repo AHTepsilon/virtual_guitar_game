9069e14cc586cc5d6ab565a447eb7ee405dc56cc
AHTepsilon <alehertoro725@gmail.com>
2022-05-25 19:07:12 -0500

    added keypoints for clickable areas

--- game/sketch.js
+++ game/sketch.js
@@ -12,5 +12,49 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
+  //console.log(mouseX + ", " + mouseY);
+
+  fill(255, 0, 0, 25);
+  rectMode(CENTER);
+
+
+  for(let i = 82; i < 861; i += 74){
+
+    for(let j = 313; j < 558; j += 46){
+
+      rect(i, j, 60, 25);
+
+    }
+
+  }
+
+  rect(139, 237, 90, 40);
+
+  circle(1000, 222, 30);
+
+  rect(92, 30, 120, 40);
+
+}
+
+function mousePressed(){
+
+  for(let i = 53; i < 860; i += 73){
+
+    for(let j = 112; j < 860 ; j += 73){
+
+        if(mouseX > i && mouseX < j && mouseY > 299 && mouseY < 326){
+
+          console.log("e string");
+
+        }
+
+    } 
+
+  }
+
+  /*if(mouseX > 53 && mouseX < 112 && mouseY > 299 && mouseY < 326){
+
+    console.log("F4");
+
+  }*/
 }
